9760974a710a5832ed46f0cca18a93a684eac20b
LuiiVasquez <luivasquez95@gmail.com>
2022-12-01 14:04:12 -0600

    removing express app

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-import express from "express";
-import morgan from "morgan";
-
 import { PORT } from "./server/config.js";
-import indexRouter from "./server/routes/index.routes.js";
-import portfolioRouter from "./server/routes/portfolio.routes.js";
 
-const app = express();
-
-app.use(morgan('dev'))
-
-app.use(indexRouter);
-app.use(portfolioRouter);
+import app from "./server/app.js";
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
