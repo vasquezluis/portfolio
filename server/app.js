3e43b4868b3416842b66cbf764aeaf368e42d50f
LuiiVasquez <luivasquez95@gmail.com>
2022-12-05 23:41:52 -0600

    changing works route name

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 import indexRouter from "./v1/routes/index.routes.js";
-import portfolioRouter from "./v1/routes/portfolio.routes.js";
+import worksRouter from "./v1/routes/works.routes.js";
 
 const app = express();
 
@@ -10,6 +10,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(indexRouter);
-app.use(portfolioRouter);
+app.use(worksRouter);
 
 export default app;
