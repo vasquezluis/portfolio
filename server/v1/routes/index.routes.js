5a51e6e6540e0a34df1acb4d883f592dc3a4be4a
LuiiVasquez <luivasquez95@gmail.com>
2022-12-05 23:44:49 -0600

    adding api menu

--- server/v1/routes/index.routes.js
+++ server/v1/routes/index.routes.js
@@ -3,7 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Listo");
+  res.json({
+    message: 'API menu',
+    menu: {
+      portfolio: `http://${req.headers.host}/api/v1/works`
+    }
+  })
 });
 
 export default router;
