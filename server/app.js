2fe908d874bb03f37e2448b5098837738fe1c3f4
LuiiVasquez <luivasque95@gmail.com>
2022-12-07 22:10:08 -0600

    adding projects routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 
 import indexRouter from "./v1/routes/index.routes.js";
 import worksRouter from "./v1/routes/works.routes.js";
+import projectsRouter from "./v1/routes/projects.routes.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use(indexRouter);
 app.use(worksRouter);
+app.use(projectsRouter);
 
 export default app;
