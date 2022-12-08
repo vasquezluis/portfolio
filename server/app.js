8cf4e1e1dcb5e9321be09ff1d76ae31bd08d12dc
LuiiVasquez <luivasque95@gmail.com>
2022-12-08 12:54:54 -0600

    adding testimonials routes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import indexRouter from "./v1/routes/index.routes.js";
 import worksRouter from "./v1/routes/works.routes.js";
 import projectsRouter from "./v1/routes/projects.routes.js";
+import testimonialsRouter from "./v1/routes/testimonials.routes.js";
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use(indexRouter);
 app.use(worksRouter);
 app.use(projectsRouter);
+app.use(testimonialsRouter);
 
 export default app;
