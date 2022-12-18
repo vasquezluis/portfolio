eabac3884f91870e28be59f046d1b934429a895f
LuiiVasquez <luivasque95@gmail.com>
2022-12-17 18:20:15 -0600

    creating works component

--- client/src/components/Works.jsx
+++ client/src/components/Works.jsx
@@ -1,36 +1,121 @@
-import { useQuery } from "react-query";
-import { getWorks } from "../api/worksAPI";
+import React from "react";
+
+import work from "../assets/projects/workImg.jpeg";
+import realestate from "../assets/projects/realestate.jpg";
 
 function Works() {
-  const {
-    isLoading,
-    data: works,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["works"],
-    queryFn: getWorks,
-  });
-
-  if (isLoading) return <div>Loading...</div>;
-  else if (works.length === 0) return <div>No hay trabajos aun</div>;
-  else if (isError) return <div>Error: {error.message}</div>;
-
-  console.log(works);
-
-  return works.map((work) => (
-    <div key={work.id}>
-      <h2>{work.title}</h2>
-      <p>{work.description}</p>
-      <img src={work.image} alt={`${work.title} photo`} />
-      <a href={work.link_1} target="_blank">Pagina</a>
-      <p>{work.link_2}</p>
-      <p>{work.tech_1}</p>
-      <p>{work.tech_2}</p>
-      <p>{work.tech_3}</p>
-      <p>{work.type}</p>
+  return (
+    <div name="works" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Works
+          </p>
+          <p className="py-6">Check out some of my recient works </p>
+        </div>
+        {/* grid container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
+          {/* card item */}
+          <div
+            style={{ backgroundImage: `url(${work})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div p-4"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* card item */}
+          <div
+            style={{ backgroundImage: `url(${realestate})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+                    {/* card item */}
+                    <div
+            style={{ backgroundImage: `url(${work})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* card item */}
+          <div
+            style={{ backgroundImage: `url(${realestate})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  ));
+  );
 }
 
 export default Works;
