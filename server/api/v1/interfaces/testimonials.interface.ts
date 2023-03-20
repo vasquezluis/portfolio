02864dc34d1fcd3270f4ab6abcc1d757b39afc3d
LuiiVasquez <luivasque95@gmail.com>
2023-03-20 17:03:57 -0600

    created testimonials interface

---
+++ server/api/v1/interfaces/testimonials.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * * testimonials interface <contract>
+ */
+
+export interface Testimonials {
+  image: string;
+  name: string;
+  job?: string;
+  testimonial: string;
+  active: boolean;
+}
