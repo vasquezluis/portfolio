9727da02320dc992016e1a1e1454435ede77867a
LuiiVasquez <luivasque95@gmail.com>
2023-03-20 12:50:58 -0600

    added getProject, getProjectByName, updateProject and deleteProject services

--- server/api/v1/services/projects.services.ts
+++ server/api/v1/services/projects.services.ts
@@ -1,6 +1,30 @@
 import ProjectModel from "../models/projects.model";
 import { Projects } from "../interfaces/projects.interface";
 
+export const getProject = async (id: string) => {
+  try {
+    const response = await ProjectModel.findById(id);
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
+};
+
+export const getProjectByName = async (name: string) => {
+  try {
+    const response = await ProjectModel.findOne({ title: name });
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
+};
+
 export const getProjects = async () => {
   try {
     const response = await ProjectModel.find({});
@@ -15,7 +39,41 @@ export const getProjects = async () => {
 
 export const createProject = async (project: Projects) => {
   try {
-    const response = await ProjectModel.create(project);
+    let newProject = { ...project, active: true };
+
+    const response = await ProjectModel.create(newProject);
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
+};
+
+export const updateProject = async (id: string, body: any) => {
+  try {
+    const response = await ProjectModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+    }
+  }
+};
+
+export const deleteProject = async (id: string) => {
+  try {
+    const response = await ProjectModel.findByIdAndUpdate(
+      id,
+      {
+        active: false,
+      },
+      { new: true }
+    );
 
     return response;
   } catch (error) {
