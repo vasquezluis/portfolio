7c7d7b9a6adb7f92849a7ced6d8bd5006cea93cb
LuiiVasquez <luivasque95@gmail.com>
2023-05-08 18:13:20 -0600

    changed about section, changed cards styles, added more technologies and TODO: contact form

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from 'react-query'
 import { getItems } from '../../api/projects'
-import ProjectCard from './ProjectCard'
+import InfoCard from '../cards/InfoCard'
 
 function Projects () {
   const { data: projects, error, isLoading, isError } = useQuery({
@@ -15,30 +15,32 @@ function Projects () {
   return (
     <>
       <div className='pt-16 pb-4 text-center'>
-        <h3 className='text-3xl md:text-4xl lg:text-5xl font-bold py-1 dark:text-seaShell'>Projects</h3>
+        <h3 className='text-3xl md:text-4xl lg:text-5xl font-bold py-1 dark:text-seaShell'>Proyectos personales</h3>
         <p className='text-md py-2 leading-8 dark:text-seaShell'>
-          Listing all the work I've done, using all
-          <span className='font-bold text-raisingBlack dark:text-atomicTangerine'> technologies</span> I know and a
-          <span className='font-bold text-raisingBlack dark:text-atomicTangerine'> picture</span> of them.
+          Lista de los proyectos personales que he realizado utilizando diferentes tecnologías.
+          {/* <span className='font-bold text-raisingBlack dark:text-atomicTangerine'> technologies</span> I know and a
+          <span className='font-bold text-raisingBlack dark:text-atomicTangerine'> picture</span> of them. */}
         </p>
       </div>
-      <div className='flex flex-col gap-2 lg:gap-5 lg:flex-row lg:flex-nowrap'>
+      <div className='flex flex-col justify-center gap-2 lg:gap-5 lg:flex-row lg:flex-nowrap'>
         {/* <div className='max-w-md rounded overflow-hidden shadow-lg'> */}
 
         {
-          projects && projects.map(item => (
-            <ProjectCard
-              key={item._id}
-              image={item.image}
-              title={item.title}
-              description={item.description}
-              web={item.link_1}
-              github={item.link_2}
-              tech1={item.tech_1}
-              tech2={item.tech_2}
-              tech3={item.tech_3}
-            />
-          ))
+          projects
+            ? projects.map(item => (
+              <InfoCard
+                key={item._id}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                web={item.link_1}
+                github={item.link_2}
+                tech1={item.tech_1}
+                tech2={item.tech_2}
+                tech3={item.tech_3}
+              />
+            ))
+            : (<h2 className='text-center text-gray-400'>Proyectos no disponibles por el momento 💔</h2>)
         }
 
         {/* </div> */}
--- client/src/components/technologies/Technologies.jsx
+++ client/src/components/technologies/Technologies.jsx
@@ -20,7 +20,7 @@ const technologiesInfo = [
 
   {
     side: 'Database',
-    technology: 'MongoDB'
+    technology: 'Mongodb'
   }
 ]
 
@@ -28,7 +28,7 @@ function Services () {
   return (
     <>
       <div className=''>
-        <h3 className='text-3xl md:text-4xl lg:text-5xl font-bold dark:text-seaShell pt-10 pb-6 md:pt-15 lg:pt-20 text-center'>Technologies I use</h3>
+        <h3 className='text-3xl md:text-4xl lg:text-5xl font-bold dark:text-seaShell pt-10 pb-6 md:pt-15 lg:pt-20 text-center'>Tecnologías que uso</h3>
       </div>
       <div className='flex flex-wrap justify-center gap-2 p-2'>
         {technologiesInfo.map((item, index) => (
