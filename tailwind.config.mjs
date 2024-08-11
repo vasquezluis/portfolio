0c8e5809fa50633c8de660705399b042b0cc7779
vasquezluis <luivasquez95@gmail.com>
2024-08-10 18:01:42 -0600

    styles: change styles and bg
    * change background color and style
    * change styles for components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,22 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				slide: "backgroundSlide 120s linear infinite alternate-reverse forwards;",
+				modeAndRotate: "moveAndRotate 5s infinite linear",
+			},
+			keyframes: {
+				backgroundSlide: {
+					"0%": { backgroundPosition: "0 0%" },
+					"100%": { backgroundPosition: "100% 50%" },
+				},
+				moveAndRotate: {
+					"0%": { transform: "translate(0, 0) rotate(0deg)" },
+					"100%": { transform: "translate(100px, 100px) rotate(360deg)" },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
