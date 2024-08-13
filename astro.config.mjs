64b02cf29eb9de4a8195799238cafeca145c2f53
vasquezluis <luivasquez95@gmail.com>
2024-08-12 21:59:39 -0600

    feat: add interactive nav menu
    + add preact module
    + add movile nav menu using preact
    * change Header component to be preact type
    * change some styles to fit responsive design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import netlify from "@astrojs/netlify"
 
+import preact from "@astrojs/preact"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   output: "server",
   adapter: netlify()
 })
